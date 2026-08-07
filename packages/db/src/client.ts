34af07d32b849fc754b17f040e32ce3c0a5ba0d2
keishingu <a157523222@gmail.com>
2026-08-07 11:45:00 +0900

    fix: Supavisorのアイドル接続を解放

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -3,11 +3,11 @@
 
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
+import { postgresClientOptions } from './client-options'
 import * as schema from './schema/index'
 
 const connectionString = process.env['DATABASE_URL']!
 
-// max: 1 でサーバーレス関数インスタンスあたりの接続数を抑制
-const client = postgres(connectionString, { prepare: false, max: 1 })
+const client = postgres(connectionString, postgresClientOptions)
 
 export const db = drizzle(client, { schema })
